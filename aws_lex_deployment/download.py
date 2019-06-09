45fb3c201cca6f0189e662a1d43540dbe2c73002
Tom Stordy-Allison <tom@stordy-allison.com>
2019-06-09 18:07:02 +0100

    Basic downloads of intent config working!

---
+++ aws_lex_deployment/download.py
@@ -0,0 +1,33 @@
+import aws_lex_deployment.bot as bot
+import aws_lex_deployment.intents as intents
+import aws_lex_deployment.intent_lambda as intent_lambda
+import sys
+import os
+import argparse
+
+#TODO Add support for version
+#TODO Add bot metadata download
+
+def download(bot_dir, instance=None):
+    ''' Download an bot from Lex, or update a local one after making changes online.'''
+    os.makedirs(bot_dir, exist_ok=True)
+
+    _, bot_name = os.path.split(os.path.normpath(bot_dir))
+    current_bot = bot.get(bot_name, instance=instance)
+    
+    intent_names = [ intent['intentName' ] for intent in current_bot['intents'] ]
+    for intent_name in intent_names:
+        intents.download(intent_name, bot_dir, instance=instance)
+        #intent_lambda.download(intent_name, bot_dir)
+    
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Download an existing bot from Lex/Lambda.')
+
+    parser.add_argument('bot_dir', help='directory containing your bot intents + lambdas')
+    parser.add_argument('--instance', dest='instance', default=None, help="add an 'instance' name to your bot, intents and lambdas")
+
+    args = parser.parse_args()
+
+    download(bot_dir=args.bot_dir, instance=args.instance)
